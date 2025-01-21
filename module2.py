73bc8fca2eced06ebf7211213099b8d00c023ef8
AkaiMak <151508656+AkaiMak@users.noreply.github.com>
2025-01-21 13:15:31 +0500

    Add files via upload

---
+++ module2.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Студент
+#
+# Created:     22.10.2024
+# Copyright:   (c) Студент 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from inspy import *
+from module1 import *
+class TestWin(QWidget):
+  def __init__(self):
+    pass
